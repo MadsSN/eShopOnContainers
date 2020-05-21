cc0be4edf80007c4c3b0a639e7df1e7d54e403e1
madssn <mads.nielsen.skytte@gmail.com>
2020-05-21 08:45:58 +0200

    Automatically update of docker files

--- src/Services/changeDockerFiles.ps1
+++ src/Services/changeDockerFiles.ps1
@@ -4,6 +4,6 @@ $to=$args[1]
 $files = Get-ChildItem -Path ../ -Recurse -Include Dockerfile
 
 Foreach($file in $files){
-	$file.FullName
-	(Get-Content $file.FullName).replace('COPY "Services/Catalog/Catalog.API/Catalog.API.csproj" "Services/Catalog/Catalog.API/Catalog.API.csproj"', 'COPY "Services/Catalog/Catalog.API/Catalog.API.csproj" "Services/Catalog/Catalog.API/Catalog.API.csproj"\nCOPY "Services/Catalog/Catalog.API/Catalog.API.csproj" "Services/Catalog/Catalog.API/Catalog.API.csproj"') | Set-Content $file.FullName
+	(Get-Content $file.FullName).replace('COPY "Services/'+$from+'/'+$from+'.API/'+$from+'.API.csproj" "Services/'+$from+'/'+$from+'.API/'+$from+'.API.csproj"', 'COPY "Services/'+$from+'/'+$from+'.API/'+$from+'.API.csproj" "Services/'+$from+'/'+$from+'.API/'+$from+'.API.csproj"
+	COPY "Services/'+$to+'/'+$to+'.API/'+$to+'.API.csproj" "Services/'+$to+'/'+$to+'.API/'+$to+'.API.csproj"') | Set-Content $file.FullName
 }
